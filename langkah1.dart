9febd6ccc91c4386f8b07b34e98d4a66a4b893d3
rstubryan <restu.bumi_ti21@nusaputra.ac.id>
2023-10-16 11:11:25 +0700

    initial commit

---
+++ langkah1.dart
@@ -0,0 +1,8 @@
+void main() {
+  String itemName = "Seblak";
+  int price = 15000;
+  double rate = 8.2;
+
+  print('Harga $itemName adalah $price');
+  print('Rata-rata orang yang suka, antara 1-10 adalah $rate');
+}
