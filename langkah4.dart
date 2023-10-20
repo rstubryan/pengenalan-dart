ab8a8879ab99d76a867bc7ff7e1d68f67e90b9af
rstubryan <restu.bumi_ti21@nusaputra.ac.id>
2023-10-20 14:41:06 +0700

    langkah 4 done

--- langkah4.dart
+++ langkah4.dart
@@ -2,17 +2,21 @@ void main() {
   List<int> data = [2, 4, 6, 8, 10, 12, 15];
 
   bool isAllEven = true;
+  bool isAllOdd = true;
 
   for (int i = 0; i < data.length; i++) {
     if (data[i] % 2 != 0) {
       isAllEven = false;
-      break;
+    } else {
+      isAllOdd = false;
     }
   }
 
-  if (isAllEven == true) {
+  if (isAllEven && !isAllOdd) {
     print("Semua elemen dalam list bernilai genap");
+  } else if (isAllOdd && !isAllEven) {
+    print("Semua elemen dalam list bernilai ganjil");
   } else {
-    print("Elemen dalam list ada yang bernilai ganjil");
+    print("Ada yang ganjil dan ada yang genap");
   }
 }
