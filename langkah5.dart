26a22b00d48cd0456f0602b2a395b5d117c896ac
rstubryan <restu.bumi_ti21@nusaputra.ac.id>
2023-10-20 14:43:23 +0700

    langkah 6 done

--- langkah5.dart
+++ langkah5.dart
@@ -13,11 +13,15 @@ void main() {
   print(setVote(voteProgramming, "Java"));
   print(setVote(voteProgramming, "Dart"));
   print(setVote(voteProgramming, "Python"));
+  print(setVote(voteProgramming, "PHP"));
+  print(setVote(voteProgramming, "Visual Basic"));
 }
 
 Map setVote(Map vp, String choice) {
   if (vp.containsKey(choice)) {
     vp[choice] = vp[choice] + 1;
+  } else {
+    vp[choice] = 1;
   }
   return vp;
 }
