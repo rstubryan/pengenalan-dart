aef59a8373b748fc6c91e6390277849dc4e2abd5
rstubryan <restu.bumi_ti21@nusaputra.ac.id>
2023-10-20 14:30:40 +0700

    langkah 2 done

--- langkah2.dart
+++ langkah2.dart
@@ -1,7 +1,14 @@
 void main() {
-  int number1 = 10;
-  var number2 = 50;
+  int number1 = 100;
+  double number2 = 50.5;
   int? number3;
 
-  int result = number1 + number2;
+  double result = number1 + number2;
+  print(result);
+
+  if (number3 != null) {
+    result += number3;
+  }
+
+  print('Hasilnya adalah = $result');
 }
