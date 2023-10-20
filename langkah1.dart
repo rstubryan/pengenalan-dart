92a0b4efbe2ee731814ed14459ca1464ffc0c691
rstubryan <restu.bumi_ti21@nusaputra.ac.id>
2023-10-20 14:29:56 +0700

    langkah 1 done

--- langkah1.dart
+++ langkah1.dart
@@ -2,7 +2,11 @@ void main() {
   String itemName = "Seblak";
   int price = 15000;
   double rate = 8.2;
+  int itemQuantity = 2;
+  int pricePerItem = 12000;
 
   print('Harga $itemName adalah $price');
   print('Rata-rata orang yang suka, antara 1-10 adalah $rate');
+  print(
+      'Jumlah $itemName yang dibeli adalah $itemQuantity dengan harga per item Rp$pricePerItem yang mempunyai rating $rate');
 }
